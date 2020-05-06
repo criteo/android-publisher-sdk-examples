9a687563a2959eb948a25fc835dac6d264bea010
Irwan Setiawan <i.setiawan@criteo.com>
2020-05-06 13:55:29 +0800

    Integrated Google Mobile Ads SDK to display Banner and Interstitial ad
    Based on Google's Android Get Started integration guide https://developers.google.com/ad-manager/mobile-ads-sdk/android/quick-start. Also applied Criteo Styles while code base is still small.
    
    Change-Id: I1573adb8c9f893666295290308555b181b0b28e8

--- appbidding_googleadmanager/src/main/java/com/criteo/publisher/samples/appbidding_googleadmanager/MainActivity.java
+++ appbidding_googleadmanager/src/main/java/com/criteo/publisher/samples/appbidding_googleadmanager/MainActivity.java
@@ -1,14 +1,50 @@
 package com.criteo.publisher.samples.appbidding_googleadmanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private PublisherAdView publisherAdView;
+  private PublisherInterstitialAd publisherInterstitialAd;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    publisherAdView = findViewById(R.id.publisherAdView);
+
+    loadInterstitial();
+
+    findViewById(R.id.displayBannerButton).setOnClickListener(v -> displayBanner());
+    findViewById(R.id.displayInterstitialButton).setOnClickListener(v -> displayInterstitial());
+  }
+
+  @Override
+  protected void onDestroy() {
+    publisherAdView.destroy();
+    super.onDestroy();
+  }
+
+  private void displayBanner() {
+    PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+    publisherAdView.loadAd(adRequest);
+  }
+
+  private void loadInterstitial() {
+    publisherInterstitialAd = new PublisherInterstitialAd(this);
+    publisherInterstitialAd.setAdUnitId("/6499/example/interstitial");
+    PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+    publisherInterstitialAd.loadAd(adRequest);
+  }
+
+  private void displayInterstitial() {
+    if (publisherInterstitialAd.isLoaded()) {
+      publisherInterstitialAd.show();
     }
+  }
 }
