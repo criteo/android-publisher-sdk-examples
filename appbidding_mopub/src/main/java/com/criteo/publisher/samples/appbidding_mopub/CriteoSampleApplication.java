070bbe1bc9fcc7e72e74250caafc1123e7e87c2e
Irwan Setiawan <i.setiawan@criteo.com>
2020-05-20 14:27:17 +0800

    Integrate MoPub SDK
    Change-Id: Idd1d4ccb9368d82f4d35b87e57d3981e420c2d59

---
+++ appbidding_mopub/src/main/java/com/criteo/publisher/samples/appbidding_mopub/CriteoSampleApplication.java
@@ -0,0 +1,10 @@
+package com.criteo.publisher.samples.appbidding_mopub;
+
+import android.app.Application;
+
+public class CriteoSampleApplication extends Application {
+
+  static final String MOPUB_BANNER_AD_UNIT_ID = "b195f8dd8ded45fe847ad89ed1d016da";
+  static final String MOPUB_INTERSTITIAL_AD_UNIT_ID = "24534e1901884e398f1253216226017e";
+
+}
--- appbidding_mopub/src/main/java/com/criteo/publisher/samples/appbidding_mopub/MainActivity.java
+++ appbidding_mopub/src/main/java/com/criteo/publisher/samples/appbidding_mopub/MainActivity.java
@@ -1,13 +1,59 @@
 package com.criteo.publisher.samples.appbidding_mopub;
 
+import static com.criteo.publisher.samples.appbidding_mopub.CriteoSampleApplication.MOPUB_BANNER_AD_UNIT_ID;
+import static com.criteo.publisher.samples.appbidding_mopub.CriteoSampleApplication.MOPUB_INTERSTITIAL_AD_UNIT_ID;
+
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.mopub.common.MoPub;
+import com.mopub.common.SdkConfiguration;
+import com.mopub.mobileads.MoPubInterstitial;
+import com.mopub.mobileads.MoPubView;
+import com.mopub.mobileads.MoPubView.MoPubAdSize;
 
 public class MainActivity extends AppCompatActivity {
+
+  private MoPubView moPubView;
+  private MoPubInterstitial moPubInterstitial;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    initMoPub();
+
+    findViewById(R.id.displayBannerButton).setOnClickListener(v -> displayBanner());
+    findViewById(R.id.displayInterstitialButton).setOnClickListener(v -> displayInterstitial());
+  }
+
+  @Override
+  protected void onDestroy() {
+    moPubInterstitial.destroy();
+    moPubView.destroy();
+    super.onDestroy();
+  }
+
+  private void initMoPub() {
+    moPubView = (MoPubView)findViewById(R.id.mopubView);
+    moPubView.setAdUnitId(MOPUB_BANNER_AD_UNIT_ID);
+    moPubView.setAdSize(MoPubAdSize.HEIGHT_50);
+
+    moPubInterstitial = new MoPubInterstitial(this, MOPUB_INTERSTITIAL_AD_UNIT_ID);
+
+    SdkConfiguration sdkConfiguration = new SdkConfiguration.Builder(MOPUB_BANNER_AD_UNIT_ID).build();
+    MoPub.initializeSdk(this, sdkConfiguration, () -> {
+      moPubInterstitial.load();
+    });
+  }
+
+  private void displayBanner() {
+    moPubView.loadAd();
+  }
+
+  private void displayInterstitial() {
+    if (moPubInterstitial.isReady()) {
+      moPubInterstitial.show();
+    }
   }
 }
