cf212d545f30f12f08f7ddd63ffe1dc65b3e8395
Irwan Setiawan <i.setiawan@criteo.com>
2023-07-31 22:37:05 +0800

    GAM Adaptive banner (#23)

--- appbidding_googleadmanager/src/main/java/com/criteo/publisher/samples/appbidding_googleadmanager/BannerActivity.java
+++ appbidding_googleadmanager/src/main/java/com/criteo/publisher/samples/appbidding_googleadmanager/BannerActivity.java
@@ -19,7 +19,9 @@ package com.criteo.publisher.samples.appbidding_googleadmanager;
 import static com.criteo.publisher.samples.appbidding_googleadmanager.CriteoSampleApplication.CRITEO_BANNER_AD_UNIT;
 import static com.criteo.publisher.samples.appbidding_googleadmanager.CriteoSampleApplication.GAM_BANNER_AD_UNIT_ID;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.WindowMetrics;
 import android.widget.FrameLayout;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,26 +29,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.criteo.publisher.Bid;
 import com.criteo.publisher.BidResponseListener;
 import com.criteo.publisher.Criteo;
+import com.criteo.publisher.model.BannerAdUnit;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.admanager.AdManagerAdRequest;
 import com.google.android.gms.ads.admanager.AdManagerAdView;
 
+import java.util.logging.Logger;
+
 public class BannerActivity extends AppCompatActivity {
 
   private AdManagerAdView adManagerAdView;
+  private FrameLayout publisherAdViewContainer;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_banner);
 
+    publisherAdViewContainer = findViewById(R.id.publisherAdViewContainer);
+
+    AdSize adSize = getAdSize();
+
     adManagerAdView = new AdManagerAdView(this);
-    adManagerAdView.setAdSizes(AdSize.BANNER);
+    adManagerAdView.setAdSizes(adSize, AdSize.BANNER);
     adManagerAdView.setAdUnitId(GAM_BANNER_AD_UNIT_ID);
-    FrameLayout publisherAdViewContainer = findViewById(R.id.publisherAdViewContainer);
     publisherAdViewContainer.addView(adManagerAdView);
 
-    displayBanner();
+    // for standard banners
+    //displayBanner(CRITEO_BANNER_AD_UNIT);
+
+    // for adaptive banners
+    displayBanner(getCriteoBannerAdUnitFromGAMAdaptiveBanner(
+            CRITEO_BANNER_AD_UNIT.getAdUnitId(),
+            adSize));
+  }
+
+  // Determine the screen width (less decorations) to use for the ad width.
+  private AdSize getAdSize() {
+    WindowMetrics windowMetrics = null;
+    Rect bounds = null;
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+      windowMetrics = getWindowManager().getCurrentWindowMetrics();
+      bounds = windowMetrics.getBounds();
+    }
+
+    float adWidthPixels = publisherAdViewContainer.getWidth();
+
+    // If the ad hasn't been laid out, default to the full screen width.
+    if (adWidthPixels == 0f) {
+      adWidthPixels = bounds.width();
+    }
+
+    float density = getResources().getDisplayMetrics().density;
+    int adWidth = (int) (adWidthPixels / density);
+
+    return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
   }
 
   @Override
@@ -55,8 +92,18 @@ public class BannerActivity extends AppCompatActivity {
     super.onDestroy();
   }
 
-  private void displayBanner() {
-    Criteo.getInstance().loadBid(CRITEO_BANNER_AD_UNIT, new BidResponseListener() {
+  // Dynamically create an instance of Criteo BannerAdUnit based on GAM adaptive banner size
+  // All of these 4 sizes must be defined in GAM line items: 300x50, 320x50, 300x100, 320x100
+  private BannerAdUnit getCriteoBannerAdUnitFromGAMAdaptiveBanner(String adUnitId, AdSize adSize) {
+    int width = adSize.getWidth() >= 320 ? 320 : adSize.getWidth() >= 300 ? 300 : adSize.getWidth();
+    int height = adSize.getHeight() >= 100 ? 100 : adSize.getHeight() >= 50 ? 50 : adSize.getHeight();
+    BannerAdUnit bannerAdUnit = new BannerAdUnit(adUnitId,
+            new com.criteo.publisher.model.AdSize(width, height));
+    return bannerAdUnit;
+  }
+
+  private void displayBanner(BannerAdUnit bannerAdUnit) {
+    Criteo.getInstance().loadBid(bannerAdUnit, new BidResponseListener() {
       @Override
       public void onResponse(@Nullable Bid bid) {
         AdManagerAdRequest.Builder builder = new AdManagerAdRequest.Builder();
